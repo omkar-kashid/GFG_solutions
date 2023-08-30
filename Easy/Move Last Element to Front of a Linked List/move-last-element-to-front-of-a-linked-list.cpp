77d07ac26ef3129160982a6cda7485e94da74793
Omkar Kashid <108173466+omkar-kashid@users.noreply.github.com>
2023-08-30 14:44:39 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Move Last Element to Front of a Linked List/move-last-element-to-front-of-a-linked-list.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode{
+public:
+    int val;
+    ListNode *next;
+    ListNode(int x){
+        val=x;
+        next=NULL;
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    ListNode *moveToFront(ListNode *head){
+        if(head==NULL) return NULL;
+        if(head->next == NULL) return head;
+        
+        ListNode *prev;
+        ListNode *curr = head;
+        
+        while(curr -> next -> next != NULL){
+            curr = curr -> next;
+        }
+        
+        prev = curr -> next;
+        curr -> next = NULL;
+        
+        prev -> next = head;
+       // head = prev;
+        return prev;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<ListNode*> a(n);
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            a[i]=new ListNode(x);
+            if(i!=0){
+                a[i-1]->next=a[i];
+            }
+        }
+        ListNode *head=a[0];
+        Solution ob;
+        head=ob.moveToFront(head);
+        while(head!=NULL){
+            cout<<head->val;
+            if(head->next!=NULL){
+                cout<<" ";
+            }
+            head=head->next;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
